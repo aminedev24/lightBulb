574ca8b1e00192ee0939a312421ce71613053845
aminedev24 <abdoufuntuber@gmail.com>
2024-09-07 00:19:28 +0200

    fixed path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/homepage';
 import About from './components/about';
@@ -8,9 +7,10 @@ import Services from './components/services';
 import Contact from './components/contact';
 import './App.css';
 import Footer from './components/footer';
+
 const App = () => {
   return (
-    <Router  basename="/lightBulb">
+    <Router>
       <Header /> {/* The Header is outside the Routes so it remains on every page */}
       <Routes>
         <Route path="/" element={<Home />} />
