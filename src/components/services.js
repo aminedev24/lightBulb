a39c6d0da1b0a40f26e64abd22aef3f721297895
aminedev24 <abdoufuntuber@gmail.com>
2024-09-08 23:10:04 +0200

    fixed path

--- src/components/services.js
+++ src/components/services.js
@@ -1,41 +1,55 @@
+// Services.jsx
 import React from 'react';
-//import serviceImage1 from '../assets/service-image1.jpg';
-//import serviceImage2 from '../assets/service-image2.jpg';
-//import serviceImage3 from '../assets/service-image3.jpg';
 import '../css/services.css';
+
 const Services = () => {
   return (
-    <>
     <section id="services" className="services-section">
       <h2>Our Services</h2>
       <div className="services-list">
         <div className="service-item">
-          <img src={"assets/switchboard.jpg"} alt="Home Appliance Installation" />
+          <img src="assets/switchboard.jpg" alt="Home Appliance Installation" />
           <h3>Home Appliance Installation</h3>
           <p>Professional installation of all types of home appliances with safety and efficiency in mind.</p>
         </div>
         <div className="service-item">
-          <img src={"assets/connectingCable.jpg"} alt="Electricity Leak Repairs" />
+          <img src="assets/connectingCable.jpg" alt="Electricity Leak Repairs" />
           <h3>Electricity Leak Repairs</h3>
           <p>Quick and effective repair services to ensure your home or office is safe from electrical hazards.</p>
         </div>
         <div className="service-item">
-          <img src={"assets/maintenance.jpg"} alt="Troubleshooting Electrical Issues" />
+          <img src="assets/maintenance.jpg" alt="Troubleshooting Electrical Issues" />
           <h3>Troubleshooting Electrical Issues</h3>
           <p>Identify and resolve all kinds of electrical issues to keep your appliances and systems running smoothly.</p>
         </div>
+        <div className="service-item">
+          <img src="assets/fixture.jpg" alt="Electrical Fixture Installation" />
+          <h3>Electrical Fixture Installation</h3>
+          <p>Install and upgrade various electrical fixtures to enhance functionality and aesthetics.</p>
+        </div>
+       
+        <div className="service-item">
+          <img src="assets/outdoor.jpg" alt="Outdoor Lighting Installation" />
+          <h3>Outdoor Lighting Installation</h3>
+          <p>Install outdoor lighting to improve visibility and security around your property.</p>
+        </div>
+        <div className="service-item">
+          <img src="assets/remodeling.jpg" alt="Remodelling" />
+          <h3>Remodelling</h3>
+          <p>Update and remodel your electrical system to meet new design and functionality needs.</p>
+        </div>
+
+
+            {/* Call to Action Section */}
+        <section className="cta-section">
+          <h2>Do you need help?</h2>
+          <p>Contact us today to schedule a service or get a free quote.</p>
+          <a href="#/contact" className="cta-button">
+            Contact Us
+          </a>
+        </section>
       </div>
     </section>
-
-     {/* Call to Action Section */}
-     <section className="cta-section">
-     <h2>Do you need help?</h2>
-     <p>Contact us today to schedule a service or get a free quote.</p>
-     <a href="#/contact" className="cta-button">
-       Contact Us
-     </a>
-   </section>
-   </>
   );
 }
 
